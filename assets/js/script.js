2d18db4ae9294c639ba16b0c4ccdd53190c0d81e
you know who <chorvickprocode@gmail.com>
2021-01-28 16:00:47 -0500

    created id currentDay to use moment to display todays date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 ///// this is where the action will take place javascript will be written here
 //// open weather api key is bfe778d8ee4fe1d07c2fd96feb41c947
 var myApiKey = "bfe778d8ee4fe1d07c2fd96feb41c947";
+////make an array for holding cities previously searched 
+var cities = [];
+/// use moment js to display todays date at top
+var momdate = moment();
+momdate = (momdate.format("MMMM Do YYYY"));
 
+var thetime = $("#currentDay");
+thetime.text(momdate);
 
 $(document).ready(function () {
 
