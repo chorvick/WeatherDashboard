b5901af40e5d21363ea36a817611c7c780000662
you know who <chorvickprocode@gmail.com>
2021-01-28 15:41:39 -0500

    have search button and text capture working - a start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,46 @@
 ///// this is where the action will take place javascript will be written here
+//// open weather api key is bfe778d8ee4fe1d07c2fd96feb41c947
+var myApiKey = "bfe778d8ee4fe1d07c2fd96feb41c947";
 
+
+$(document).ready(function () {
+
+
+
+    $("#btn").click(function () {
+        var txt = $("#form1").val();
+        console.log(txt);
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
